1e9264ab00c68fc86428ff750f58c22526e08596
folkert van heusden <mail@vanheusden.com>
2024-06-17 20:23:11 +0200

    circle animation

--- arduino/lightbox.ino
+++ arduino/lightbox.ino
@@ -239,6 +239,9 @@ void cls()
 
 void set_pixel(const int x, const int y, const bool c)
 {
+	if (x >= 64 || x < 0 || y >= 24 || y < 0)
+		return;
+
 	int pixel_row = y / 8;
 	int matrix    = x / 8;
 	int matrix_y  = 7 - (y & 7);
@@ -252,9 +255,81 @@ void set_pixel(const int x, const int y, const bool c)
 		data[o] &= ~mask;
 }
 
+bool get_pixel(const int x, const int y)
+{
+	if (x >= 64 || x < 0 || y >= 24 || y < 0)
+		return false;
+
+	int pixel_row = y / 8;
+	int matrix    = x / 8;
+	int matrix_y  = 7 - (y & 7);
+	int matrix_x  = x & 7;
+	int o         = pixel_row * 64 + matrix * 8 + matrix_y;
+	int mask      = 1 << matrix_x;
+
+	return !!(data[o] & mask);
+}
+
+int get_distance(int x1, int y1, int x2, int y2)
+{
+	return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+}
+
+std::pair<int, int> get_next_point(int x, int y, int dx, int dy, int cx, int cy, int r)
+{
+	int r2 = r * r;
+
+	int x1 = x + dx;
+	int y1 = y + dy;
+
+	int x2 = x;
+	int y2 = y + dy;
+
+	int x3 = x + dx;
+	int y3 = y;
+
+	int dif1 = abs(get_distance(x1, y1, cx, cy) - r2);
+	int dif2 = abs(get_distance(x2, y2, cx, cy) - r2);
+	int dif3 = abs(get_distance(x3, y3, cx, cy) - r2);
+
+	int diff_min = std::min(std::min(dif1, dif2), dif3);
+
+	if (diff_min == dif1)
+		return { x1, y1 };
+	else if (diff_min == dif2)
+		return { x2, y2 };
+
+	return { x3, y3 };
+}
+
+void get_quadrant(int bx, int by, int dx, int dy, int cx, int cy, int r, std::vector<std::pair<int, int> > *const out)
+{
+	int x = bx;
+	int y = by;
+
+	int max_x = bx + dx * r;
+	int max_y = by + dy * r;
+
+	while ((dx * (x - max_x) <= 0) && (dy * (y - max_y) <= 0)) {
+		auto rc = get_next_point(x, y, dx, dy, cx, cy, r);
+		x = rc.first;
+		y = rc.second;
+
+		out->push_back(rc);
+	}
+}
+
+void circle(int r, int cx, int cy, std::vector<std::pair<int, int> > *const out)
+{
+	get_quadrant(cx, cy - r, 1, 1, cx, cy, r, out);
+	get_quadrant(cx + r, cy, -1, 1, cx, cy, r, out);
+	get_quadrant(cx, cy + r, -1, -1, cx, cy, r, out);
+	get_quadrant(cy - r, cy, 1, -1, cx, cy, r, out);
+}
+
 void animate(int mode)
 {
-mode = 2; // TODO
+mode = 3;
 	if (mode == 1) {
 		static int y = 0;
 		static int d = 1;
@@ -290,6 +365,17 @@ mode = 2; // TODO
 		}
 	}
 	else if (mode == 3) {
+		static std::vector<std::pair<int, int> > pixels[4];
+		static int pixels_nr = 0;
+
+		pixels[pixels_nr].clear();
+		circle(1 + (rand() % 64), rand() & 63, rand() % 24, &pixels[pixels_nr++]);
+		pixels_nr &= 3;
+
+		for(int i=0; i<4; i++) {
+			for(auto & p: pixels[i])
+				set_pixel(p.first, p.second, !get_pixel(p.first, p.second));
+		}
 	}
 	else {
 		cls();
@@ -310,7 +396,7 @@ void loop() {
 	static int mode = 0;
 
 	int packetSize = Udp.parsePacket();
-	if (!packetSize) {
+	if (packetSize == 0) {
 		if (now - prev >= 1500) {
 			if (mode == 0)
 				mode = (rand() % 3) + 1;
