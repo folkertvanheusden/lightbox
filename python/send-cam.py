82ed8bdf1c5b806ba9d26a1d3ddf5439b2583637
Folkert van Heusden <folkert@vanheusden.com>
2026-02-14 09:10:42 +0100

    cam viewer

--- python/send-cam.py
+++ python/send-cam.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 
-# (C) 2019 by Folkert van Heusden <mail@vanheusden.com>
+# (C) 2019-2024 by Folkert van Heusden <mail@vanheusden.com>
 
 # This script requires the "opencv-python" package.
 
@@ -8,11 +8,8 @@ from lzjb import compress
 import socket
 import time
 
-#UDP_IP = '192.168.65.212' # change this
-#UDP_IP = '10.208.42.56' # change this
-#UDP_IP = '192.168.65.150'
-UDP_IP = '10.42.45.47'
-UDP_PORT = 32001
+MC_IP = '226.1.1.9'
+MC_PORT = 32009
 
 from cv2 import *
 # initialize the camera
@@ -26,41 +23,38 @@ def rescale_by_width(image, target_width, method=INTER_LANCZOS4):
 def set_pixel(im,x,y,new):
     im[y,x] = new
 
-while True:
-    s, img = cam.read()
+MULTICAST_TTL = 15
 
-    if not s:
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+
+while True:
+    sa, img = cam.read()
+    if not sa:
         continue
 
     img = rescale_by_width(img, 64)
 
     avg = 0
-
-    for y in range(0, 24):
-        for x in range(0, 64):
-            avg += img[y][x][0]
-            avg += img[y][x][1]
-            avg += img[y][x][2]
-
+    for y in range(24):
+        for x in range(64):
+            avg += int(img[y][x][0])
+            avg += int(img[y][x][1])
+            avg += int(img[y][x][2])
     avg /= 24 * 64 * 3
-    print(avg)
 
     MESSAGE = bytearray()
-
     for d in range(0, 3):
         for x in range(0, 64, 8):
             for y in range(0, 8):
                 yy = 7 - y + d  * 8
-
                 b = 0
                 for xx in range(0, 8):
                     b <<= 1
-
                     if (int(img[yy, x + 7 - xx][0]) + int(img[yy, x + 7 - xx][1]) + int(img[yy, x + 7 - xx][2])) / 3 >= avg:
                         b |= 1
-
                 MESSAGE.append(b)
-
     data_lzjb = compress(MESSAGE)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(data_lzjb, (UDP_IP, UDP_PORT))
+    s.sendto(data_lzjb, (MC_IP, MC_PORT))
+
+    time.sleep(0.1)
