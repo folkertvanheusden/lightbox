48a166f915c837d2a1f9c6c8b0f922f767e1a0e3
Folkert van Heusden <folkert@vanheusden.com>
2026-02-18 08:03:45 +0100

    clean-up

--- arduino/lightbox.ino
+++ arduino/lightbox.ino
@@ -21,31 +21,17 @@
 
 #include "favicon.h"
 #include "font.h"
+#include "pixelflood.h"
+#include "settings.h"
 #include "simple-css.h"
-
-// #define DEBUG
-#define SCREENSAVER_START   2100
-#define SCREENSAVER_ROTATE  15000
-#define WIDTH               64
-#define HEIGHT              24
-#define DDP_PORT            4048
-#define PIXELFLOOD_TXT_PORT 1337
-#define TEXT_PORT           5001
-#define EEPROM_SIZE         4096
-#define LRC_INIT            0x91
-#define BOX_WIDTH           28    // physical dimension, in cm, for BMP
-#define PIXELFLOOD_ANNOUNCE_INTERVAL 1500
-#define PIXELFLOOD_BIN_PORT          5000
-#define PIXELFLOOD_BIN_ANNOUNCE_PORT 5006
-#define DDP_ANNOUNCE_INTERVAL        1500
 
 WiFiUDP    udpMC;  // multicast LZJB compressed bitmap (64x24)
 WiFiServer tcpTxtPixelfloodServer(PIXELFLOOD_TXT_PORT);
 WiFiUDP    udpTxtPixelfloodServer;
 WiFiUDP    udpBinPixelfloodServer;
+WiFiUDP    udpAnnounceBinPixelflood;
 WiFiUDP    udpDdp;
 WiFiUDP    udpText;
-WiFiUDP    udpAnnounceBinPixelflood;
 uint8_t    broadcast[4] { };
 
 char       mqtt_server       [64] { "vps001.komputilo.nl"    };
@@ -54,15 +40,6 @@ char       mqtt_bitmap_topic [64] { "nurdspace/hek42tocker"  };
 char       mqtt_on_topic     [64] { "nurdspace/on_off_notif" };
 char       mqtt_on_topic_full[98];
 uint16_t   mqtt_port              { 1883                     };
-
-#define BS  48
-struct pf {
-  WiFiClient handle;
-  int        o          { 0       };
-  char       buffer[BS] {         };
-};
-
-std::vector<pf> pfClients;
 
 static bool in_ota = false;
 
@@ -93,7 +70,7 @@ bool enable_ddp         = true;
 bool enable_text_anim   = true;
 
 uint8_t work_buffer[4608];  // enough to fit a BMP in
-char   *const p = reinterpret_cast<char *>(work_buffer);
+char   *p = reinterpret_cast<char *>(work_buffer);
 uint8_t data[192];
 
 void putScreen() {
@@ -919,296 +896,6 @@ bool setPixelChecked(const unsigned x, const unsigned y, const bool c) {
   }
   setPixel(x, y, c);
   return true;
-}
-
-bool processTxtPixelfloodPixel(const char *const buf, const char *const buf_end) {
-  if (buf[0] != 'P' || buf[1] != 'X' || buf[2] != ' ') {
-#if defined(DEBUG)
-    Serial.println(F("No PX header"));
-#endif
-    return false;
-  }
-
-  const char *sp[3] { nullptr, nullptr, nullptr };
-  int n_sp = 0;
-  const char *pb = buf;
-  while(pb < buf_end && n_sp < 3) {
-    if (*pb == ' ')
-      sp[n_sp++]= pb;
-    pb++;
-  }
-  if (n_sp != 3) {
-#if defined(DEBUG)
-    Serial.println(F("Field missing"));
-#endif
-    return false;
-  }
-
-  int x = atoi(sp[0]);
-  int y = atoi(sp[1]);
-
-  if (buf_end - sp[2] < 6) {
-#if defined(DEBUG)
-    Serial.println(F("RGB invalid?"));
-#endif
-    return false;
-  }
-
-  // evaluate r, g and b: only upper nibble
-  int  r     = 0;
-  char n1    = toupper(sp[2][1]);
-  if (n1 >= 'A')
-    r = n1 - 'A' + 10;
-  else
-    r = n1 - '0';
-
-  int  g      = 0;
-  char n2     = toupper(sp[2][3]);
-  if (n2 >= 'A')
-    g = n2 - 'A' + 10;
-  else
-    g = n2 - '0';
-
-  int  b      = 0;
-  char n3     = toupper(sp[2][5]);
-  if (n3 >= 'A')
-    b = n3 - 'A' + 10;
-  else
-    b = n3 - '0';
-
-  bool c = (r + g + b) >= 8 * 3;
-  return setPixelChecked(x, y, c);
-}
-
-bool processTxtPixelflood(size_t nr) {
-  for(;;) {
-    char *buf = pfClients.at(nr).buffer;
-    char *lf  = strchr(buf, '\n');
-    if (lf == nullptr)
-      return true;
-    *lf = 0x00;
-
-    if (strcmp(buf, "SIZE") == 0) {
-      char buffer[16];
-      snprintf(buffer, sizeof buffer, "SIZE %d %d\n", WIDTH, HEIGHT);
-      pfClients.at(nr).handle.print(buffer);
-#if defined(DEBUG)
-      Serial.println(F("SIZE request"));
-#endif
-    }
-    else if (lf - buf < 13) {
-#if defined(DEBUG)
-      Serial.println(F("PX request too small"));
-#endif
-      return false;
-    }
-    else if (!processTxtPixelfloodPixel(buf, lf)) {
-#if defined(DEBUG)
-      Serial.println(F("processTxtPixelfloodPixel failed"));
-#endif
-      return false;
-    }
-
-    int was_length = lf - pfClients[nr].buffer + 1;
-    int bytes_left = pfClients[nr].o - was_length;
-    if (bytes_left < 0) {  // internal error
-#if defined(DEBUG)
-      Serial.println(F("processTxtPixelflood internal error"));
-#endif
-      return false;
-    }
-
-    if (bytes_left > 0)
-      memmove(&pfClients[nr].buffer[0], lf + 1, bytes_left);
-    pfClients[nr].o -= was_length;
-  }
-
-  return false;
-}
-
-// https://github.com/JanKlopper/pixelvloed/blob/master/protocol.md
-bool processBinPixelflood(const uint16_t len) {
-  if (len < 2) {
-#if defined(DEBUG)
-    Serial.println(F("Packet too shot"));
-#endif
-    return false;
-  }
-  const uint8_t *const pb    = work_buffer;
-  const bool           alpha = work_buffer[1] & 1;
-
-  if (pb[0] == 0) {
-    for(uint16_t i=2; i<len; i += (alpha ? 8 : 7)) {
-      uint16_t x = pb[i + 0] + (pb[i + 1] << 8);
-      uint16_t y = pb[i + 2] + (pb[i + 3] << 8);
-      uint8_t  g = (pb[i + 4] + pb[i + 5] + pb[i + 6]) / 3;
-      if (!setPixelChecked(x, y, g >= 128))
-        return false;
-    }
-  }
-  else if (pb[0] == 1) {
-    for(uint16_t i=2; i<len; i += (alpha ? 7 : 6)) {
-      uint16_t x = pb[i + 0] + ((pb[i + 1] & 0x0f) << 8);
-      uint16_t y = (pb[i + 1] >> 4) + (pb[i + 2] << 4);
-      uint8_t  g = (pb[i + 3] + pb[i + 4] + pb[i + 5]) / 3;
-      if (!setPixelChecked(x, y, g >= 128))
-        return false;
-    }
-  }
-  else if (pb[0] == 2) {
-    for(uint16_t i=2; i<len; i += 4) {
-      uint16_t x    = pb[i + 0] + ((pb[i + 1] & 0x0f) << 8);
-      uint16_t y    = (pb[i + 1] >> 4) + (pb[i + 2] << 4);
-      uint8_t  temp = pb[i];
-      bool     c    = ((temp >> 6) + ((temp >> 4) & 3) + ((temp >> 2) & 3) + (temp & 3)) >= 2;
-      if (!setPixelChecked(x, y, c))
-        return false;
-    }
-  }
-  else if (pb[0] == 3) {
-    uint8_t temp = pb[1];
-    bool    c    = ((temp >> 6) + ((temp >> 4) & 3) + ((temp >> 2) & 3) + (temp & 3)) >= 2;
-    for(uint16_t i=2; i<len; i += 3) {
-      uint16_t x = pb[i + 0] + ((pb[i + 1] & 0x0f) << 8);
-      uint16_t y = (pb[i + 1] >> 4) + (pb[i + 2] << 4);
-      if (!setPixelChecked(x, y, c))
-        return false;
-    }
-  }
-  else {
-#if defined(DEBUG)
-  Serial.println(F("Invalid protocol"));
-#endif
-    return false;
-  }
-
-  return true;
-}
-
-void sendBinPixelfloodAnnouncement() {
-  static uint32_t prev_send = 0;
-	uint32_t        now       = millis();
-  if (now - prev_send < PIXELFLOOD_ANNOUNCE_INTERVAL)
-    return;
-  prev_send = now;
-  auto ip = WiFi.localIP();
-  snprintf(p, sizeof work_buffer, "pixelvloed:1.00 %d.%d.%d.%d:%d %d*%d", ip[0], ip[1], ip[2], ip[3], PIXELFLOOD_BIN_PORT, WIDTH, HEIGHT);
-#if defined(DEBUG)
-  Serial.println(p);
-#endif
-  udpAnnounceBinPixelflood.beginPacket(broadcast, PIXELFLOOD_BIN_ANNOUNCE_PORT);
-  udpAnnounceBinPixelflood.write(p);
-  udpAnnounceBinPixelflood.endPacket();
-}
-
-std::pair<bool, bool> processPixelfloodStreams() {
-  bool activity       = false;
-  bool drawn_anything = false;
-
-  sendBinPixelfloodAnnouncement();
-
-  // pixelflood connection management
-  WiFiClient newPixelfloodClient = tcpTxtPixelfloodServer.available();
-  if (newPixelfloodClient) {
-#if defined(DEBUG)
-    Serial.println(F("new TCP client"));
-#endif
-    // check if all still there
-    for(size_t i=0; i<pfClients.size();) {
-      if (pfClients.at(i).handle.connected() == false) {
-        pfClients.erase(pfClients.begin() + i);
-      }
-      else {
-        i++;
-      }
-    }
-    // max 32 clients
-    while(pfClients.size() > 32) {
-      pfClients.erase(pfClients.begin());
-      Serial.println(F("CLOSE SESSION"));
-    }
-
-    pfClients.push_back({ WiFiClient(newPixelfloodClient), 0 });
-
-    activity = true;
-  }
-
-  // check pixelflood clients for data
-  for(size_t i=0; i<pfClients.size(); i++) {
-    int nAvail = pfClients[i].handle.available();
-    if (nAvail == 0)
-      continue;
-    activity = true;
-    // read data from socket until \n is received (a complete pixelflood "packet")
-    bool fail = false;
-    for(int nr=0; nr<nAvail; nr++) {
-      // read & add to buffer unless it is still full (when full, it is invalid)
-      int c = pfClients[i].handle.read();
-      if (c == -1)
-        fail = true;
-      else if (pfClients[i].o >= BS) {  // sanity check
-#if defined(DEBUG)
-        Serial.printf("too much data %d %d\r\n", pfClients[i].o, BS);
-        Serial.println(pfClients[i].buffer);
-#endif
-        fail = true;
-      }
-      else {
-        pfClients[i].buffer[pfClients[i].o++] = char(c);
-      }
-
-      if (c == '\n') {
-        pfClients[i].buffer[BS - 1] = 0x00;
-        if (processTxtPixelflood(i))
-          drawn_anything = true;
-        else
-          fail = true;
-      }
-
-      if (fail) {
-        Serial.println(F("FAIL"));
-        pfClients[i].handle.stop();
-        break;
-      }
-    }
-  }
-
-  // check UDP text pixelflood
-  int packetSizeTxt = udpTxtPixelfloodServer.parsePacket();
-  if (packetSizeTxt) {
-    int len = udpTxtPixelfloodServer.read(work_buffer, sizeof(work_buffer) - 1);
-#if defined(DEBUG)
-    Serial.printf("UDPTXT: %d, %s\r\n", len, p);
-#endif
-    work_buffer[len] = 0x00;
-    char *work_p = p;  // p is a char-pointer to work_buffer
-    for(;;) {
-      char *lf = strchr(work_p, '\n');
-      if (!lf)
-        break;
-      if (processTxtPixelfloodPixel(work_p, lf) == false)
-        break;
-      work_p = lf + 1;
-    }
-
-    drawn_anything = true;
-    activity       = true;
-  }
-
-  // check UDP bin pixelflood
-  int packetSizeBin = udpBinPixelfloodServer.parsePacket();
-  if (packetSizeBin) {
-    uint16_t len = udpBinPixelfloodServer.read(work_buffer, sizeof work_buffer);
-#if defined(DEBUG)
-    Serial.printf("UDPBIN: %d\r\n", len);
-#endif
-    if (processBinPixelflood(len)) {
-      drawn_anything = true;
-      activity       = true;
-    }
-  }
-
-  return { activity, drawn_anything };
 }
 
 std::pair<bool, bool> processDdpStream() {
---
+++ arduino/settings.h
@@ -0,0 +1,15 @@
+// #define DEBUG
+#define SCREENSAVER_START   2100
+#define SCREENSAVER_ROTATE  15000
+#define WIDTH               64
+#define HEIGHT              24
+#define DDP_PORT            4048
+#define PIXELFLOOD_TXT_PORT 1337
+#define TEXT_PORT           5001
+#define EEPROM_SIZE         4096
+#define LRC_INIT            0x91
+#define BOX_WIDTH           28    // physical dimension, in cm, for BMP
+#define PIXELFLOOD_ANNOUNCE_INTERVAL 1500
+#define PIXELFLOOD_BIN_PORT          5000
+#define PIXELFLOOD_BIN_ANNOUNCE_PORT 5006
+#define DDP_ANNOUNCE_INTERVAL        1500
